d510b51e157836dba3b3e7c778ac6bf2b0008bef
Tiago Garcia <tiago.tiago22@gmail.com>
2017-07-04 00:40:07 -0300

    Part One Solution

--- p1/main.rb
+++ p1/main.rb
@@ -1,20 +1,23 @@
-def get_day(line)
+def day_column(line)
   line.split(" ")[0].to_i
 end
 
-def get_min(line)
+def max_column(line)
   line.split(" ")[1].to_i
 end
 
-def get_max(line)
+def min_column(line)
   line.split(" ")[2].to_i
 end
 
-def valid_line?(line)
-  line.split(" ")[0].to_i != 0
+def valid_row?(line)
+  line.split(" ")[0].to_i > 0 &&
+    line.split(" ")[0].to_i <= 30 &&
+    min_column(line) > 0 &&
+    max_column(line) > 0
 end
 
-def spread(min_temperature, max_temperature)
+def spread(max_temperature, min_temperature)
   max_temperature - min_temperature
 end
 
@@ -24,24 +27,22 @@ min_spread = 100000000000
 min_day = 0
 
 while(line = file.gets)
-  puts("x"*50)
+  line = line.gsub(/[^0-9,.]/, ' ')
 
-  if valid_line?(line)
+  if valid_row?(line)
 
-    day = get_day(line)
-    min = get_min(line)
-    max = get_max(line)
+    min = min_column(line)
+    max = max_column(line)
 
-    current_spread = spread(min, max)
+    current_spread = spread(max, min)
 
     if current_spread < min_spread
-      min_day = day
+      min_spread = current_spread
+      min_day    = day_column(line)
     end
-
-    puts(line)
   end
 end
 
-puts(day)
+puts("Min day is #{min_day}")
 
 file.close
