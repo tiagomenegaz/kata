62f43c46a5747a82449def8fa56fef3978d51fd2
Tiago Garcia <tiago.tiago22@gmail.com>
2017-08-07 23:23:15 -0300

    Solution with specs

--- p1/main.rb
+++ p1/main.rb
@@ -1,14 +1,48 @@
-File.open("weather.dat", "r") do |file|
-  min_spread = 100000000000
-  min_day = 0
-  file.each_line do |line|
-    elements = line.gsub(/[^0-9,.]/, ' ').split[0..2].map(&:to_i)
-    next if elements.size != 3 || elements[0] > 31
+def day_column(line)
+  line.split(" ")[0].to_i
+end
 
-    if (elements[1] - elements[2]) < min_spread
-      min_spread = elements[1] - elements[2]
-      min_day = elements[0]
+def max_column(line)
+  line.split(" ")[1].to_i
+end
+
+def min_column(line)
+  line.split(" ")[2].to_i
+end
+
+def valid_row?(line)
+  line.split(" ")[0].to_i > 0 &&
+    line.split(" ")[0].to_i <= 30 &&
+    min_column(line) > 0 &&
+    max_column(line) > 0
+end
+
+def spread(max, min)
+  max - min
+end
+
+file = File.open("weather.dat", "r")
+
+min_spread = 100000000000
+min_day = 0
+
+while(line = file.gets)
+  line = line.gsub(/[^0-9,.]/, ' ')
+
+  if valid_row?(line)
+
+    min = min_column(line)
+    max = max_column(line)
+
+    current_spread = spread(max, min)
+
+    if current_spread < min_spread
+      min_spread = current_spread
+      min_day    = day_column(line)
     end
   end
-  puts "#{min_day}"
 end
+
+puts("Min day is #{min_day}")
+
+file.close
---
+++ p2/test/output_aston_villa_spec.rb
@@ -0,0 +1,15 @@
+require_relative "spec_helper"
+
+describe "main.rb with sample file" do
+
+  it "prints Aston Villa to STDOUT" do
+    output_for_main.must_equal <<~EOF
+    Team is Aston_Villa
+    EOF
+  end
+
+  def output_for_main
+    %x(ruby main.rb)
+  end
+
+end
