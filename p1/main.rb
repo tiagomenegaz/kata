f8c53cbba01d04aa8733f79c6163bb2c671507a7
Tiago Garcia <tiago.tiago22@gmail.com>
2017-08-29 14:37:31 -0300

    Seventh solution

--- p1/main.rb
+++ p1/main.rb
@@ -1,19 +1,6 @@
 require "./../processor"
 
-File.open("weather.dat", "r") do |file|
-  min_spread = 100000000000
-  min_day = 0
-  file.each_line do |line|
-    next if Processor.line_value_at(line, 0) == 0
-
-    max = Processor.line_value_at(line, 1)
-    min = Processor.line_value_at(line, 2)
-    current_spread = Processor.difference(max, min)
-
-    if (current_spread) < min_spread
-      min_spread = current_spread
-      min_day    = Processor.line_element_at(line, 0)
-    end
-  end
-  puts "#{min_day}"
-end
+elements  = File.open("weather.dat", "r").map { |line| line }
+processor = Processor.new(elements, nil, primary: 1, secondary: 2, output_index: 0)
+processor.run
+puts "#{processor.result}"
--- p2/main.rb
+++ p2/main.rb
@@ -1,19 +1,6 @@
 require "./../processor"
 
-File.open("football.dat", "r") do |file|
-  min_spread = 100000000000
-  team = ""
-  file.each_line do |line|
-    next if line.split(' ').size != 10
-
-    for_goal   = Processor.line_value_at(line, -4)
-    for_againt = Processor.line_value_at(line, -2)
-    diff       = Processor.difference(for_goal, for_againt)
-
-    if diff < min_spread
-      min_spread = diff
-      team = Processor.line_element_at(line, 1)
-    end
-  end
-  puts "Team is #{team}"
-end
+elements = File.open("football.dat", "r").map { |line| line }
+processor = Processor.new(elements, nil, primary: -4, secondary: -2, output_index: 1)
+processor.run
+puts "Team is #{processor.result}"
